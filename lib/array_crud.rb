8f0318a9b6e4b756d8fe1d69e5944a0cc5a34913
Steven Ross <ross.steven@me.com>
2020-04-10 17:12:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -26,7 +26,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-    my_array = [" ", "am ", " ", " "]
+    my_array = [" ", "am", " ", " "]
     my_array[2]
 end
 
